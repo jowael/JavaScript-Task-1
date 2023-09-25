7d48a6b20045499005f3f2b5522da6d9f8d3c10b
jowael <yousefwail22@gmail.com>
2023-09-25 23:03:12 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function addToCart(productName, price) {
+  const cartSection = document.getElementById("cart-section");
+  const cartItem = document.createElement("div");
+  cartItem.classList.add("product-card-cart");
+  cartItem.innerHTML = `
+      <p>Name: ${productName}</p>
+      <p>Price: $${price.toFixed(2)}</p>
+  `;
+  cartSection.appendChild(cartItem);
+}
+
+function showTotalPrice() {
+  const cartItems = document.querySelectorAll("#cart-section .product-card");
+  let totalPrice = 0;
+
+  cartItems.forEach((item) => {
+    const priceText = item.querySelector("p:nth-child(2)").textContent;
+    const price = parseFloat(priceText.replace("Price: $", ""));
+    totalPrice += price;
+  });
+
+  const totalPriceElement = document.getElementById("total-price");
+  totalPriceElement.textContent = `Total Price: $${totalPrice.toFixed(2)}`;
+}
